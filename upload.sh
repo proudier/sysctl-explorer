18643a0ba7684ef6936c0b62fc9d6932beb45384
Pierre ROUDIER <pierre@roudier.io>
2018-07-16 16:19:41 +0200

    changed aws profile name

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-bundle exec jekyll clean 
-&& bundle exec jekyll build 
-&& aws --profile pjwr s3 sync --delete _site/ s3://sysctl-explorer-prd 
-&& bundler exec jekyll algolia 
-&& wget --recursive --directory-prefix='/tmp/' https://sysctl-explorer.net
-&& rm --fr /tmp/sysctl-explorer.net/
+bundle exec jekyll clean \
+&& bundle exec jekyll build \
+&& aws --profile roudier s3 sync --delete _site/ s3://sysctl-explorer-prd \
+&& bundler exec jekyll algolia \
+&& wget --recursive --directory-prefix='/tmp/' https://sysctl-explorer.net \
+&& rm --fr /tmp/sysctl-explorer.net/ \
